2a516701b47a522610c4451194081700e24f3943
Sumitkumar104 <parjapatsumit104@gmail.com>
2023-07-12 02:04:19 +0530

    first commit

---
+++ src/Gallary.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Gallary.css';
+// import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+function Gallary() {
+  return (
+    <div className='gallary-container'>
+
+      <div className="home_content">
+
+        <div className="home_heading-1">Welcome To</div>
+        <div className="home_heading-2">Art Gallery of EMR Club</div>
+         
+      </div>
+
+    </div>
+  )
+}
+
+export default Gallary
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Gallary from './Gallary';
+
+// import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+   <Gallary/>
+  </React.StrictMode>
+);
+
+
